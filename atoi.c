0af9d10899d487eb5b629f492d5a2dfd496be0c6
solamsalu <solamsalu@gmail.com>
2023-02-21 02:48:30 -0800

    atoi

--- atoi.c
+++ atoi.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is in interactive mode
+ * interactive - return true if shell is in interactive mode
  * @info: struct address
  * Return: 1 if interactive mode, else 0
  */
