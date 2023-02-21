f74f754c7875ca96e23dc008c84519f737db1049
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-02-21 13:58:14 +0300

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -4,7 +4,7 @@
  * input_buf - buffers chained commands
  * @info: parameter struct
  * @buf: address of buffer
- * @len: address of len var
+ * @len: address of len var 
  * Return: bytes read
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
