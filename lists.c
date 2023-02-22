323b846b3ba3e58271caeadea7f34b675cc71f55
solamsalu <solamsalu@gmail.com>
2023-02-22 01:55:32 -0800

    updated

--- lists.c
+++ lists.c
@@ -5,7 +5,7 @@
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
- * 
+ *
  * Return: size of list
  */
 list_t *add_node(list_t **head, const char *str, int num)
